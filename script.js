d32e23eb40c95883baf5cad4a3f9553054fe4285
Edu Costa <eduardomc.devs@gmail.com>
2023-01-18 13:32:31 -0300

    JS array - forEach and IMG

---
+++ script.js
@@ -0,0 +1,13 @@
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('el.__close-btn'));
+
+
+setTimeout(function(){
+    $cont.classList.remove('s--inactive');
+
+}, 200);
+
+$elsArr.array.array.forEach(element => {
+    
+});
